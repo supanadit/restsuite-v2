8a5ad9441f45609e130a95a442f25b1e205229d9
Supan Adit Pratama <supanadit@gmail.com>
2022-09-08 22:27:39 +0700

    wip: clean way to call repository

--- src/main/kotlin/feature/rest/RestModule.kt
+++ src/main/kotlin/feature/rest/RestModule.kt
@@ -2,19 +2,12 @@ package feature.rest
 
 import dagger.Module
 import dagger.Provides
-import feature.rest.data.datasources.RestLocalDataSource
-import feature.rest.data.datasources.RestRemoteDataSource
 import feature.rest.data.repositories.RestRepositoryImpl
 import feature.rest.presentation.panel.RestPanel
 import javax.inject.Singleton
 
 @Module
 class RestModule {
-
-    @Provides
-    fun restRepositoryImpl(restLocalDataSource: RestLocalDataSource, restRemoteDataSource: RestRemoteDataSource) =
-        RestRepositoryImpl(restLocalDataSource, restRemoteDataSource)
-
     @Provides
     @Singleton
     fun restPanel(restRepositoryImpl: RestRepositoryImpl) = RestPanel(restRepositoryImpl)
